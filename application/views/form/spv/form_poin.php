7bab37006e79f9dd4e00a0955a1919fa180a2728
ramaadtym <ramaadtym@gmail.com>
2018-05-08 13:13:23 +0700

    Tambah Fitur Lihat Progress di laman SPV

--- application/views/form/spv/form_poin.php
+++ application/views/form/spv/form_poin.php
@@ -7,10 +7,10 @@
         </button>
       </h5>
     </div>
-
+    <?php echo form_open('');?>
     <div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordion">
       <div class="card-body">
-        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+        
       </div>
     </div>
   </div>
@@ -18,7 +18,7 @@
     <div class="card-header" id="headingTwo">
       <h5 class="mb-0">
         <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-          Tanggung Jawab
+          Profesional
         </button>
       </h5>
     </div>
@@ -32,7 +32,7 @@
     <div class="card-header" id="headingThree">
       <h5 class="mb-0">
         <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-          Inovasi dan Kreatifitas
+          Tanggung Jawab
         </button>
       </h5>
     </div>
@@ -43,3 +43,4 @@
     </div>
   </div>
 </div>
+<?php echo form_close();?>
