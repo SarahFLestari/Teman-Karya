837c38a440922c7e267ced47927e538bf79a95e2
ramaadtym <ramaadtym@gmail.com>
2018-05-08 13:30:22 +0700

    Tambah view add poin spv

--- application/views/form/spv/form_poin.php
+++ application/views/form/spv/form_poin.php
@@ -1,46 +1,47 @@
+<?php echo form_open('');?>
 <div id="accordion">
-  <div class="card">
-    <div class="card-header" id="headingOne">
-      <h5 class="mb-0">
-        <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-          Kedisiplinan
-        </button>
-      </h5>
-    </div>
-    <?php echo form_open('');?>
-    <div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordion">
-      <div class="card-body">
-        
-      </div>
-    </div>
-  </div>
   <div class="card">
     <div class="card-header" id="headingTwo">
       <h5 class="mb-0">
-        <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+        <a class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
           Profesional
-        </button>
+        </a>
       </h5>
     </div>
     <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
-      <div class="card-body">
-        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+      <div class="card-body ml-3">
+       <div class="form-check">
+         <input class="form-check-input" type="checkbox" value="10" name="ontime" id="defaultCheck1">
+         <label class="form-check-label" for="defaultCheck1">
+           Bekerja sesuai dengan deadline yang ditentukan
+         </label>
+       </div>
+       <div class="form-check">
+         <input class="form-check-input" type="checkbox" value="10" name="lapor" id="defaultCheck1">
+         <label class="form-check-label" for="defaultCheck1">
+           Memberikan laporan progress sesuai dengan waktu yang ditentukan
+         </label>
+       </div>
       </div>
     </div>
   </div>
   <div class="card">
     <div class="card-header" id="headingThree">
       <h5 class="mb-0">
-        <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-          Tanggung Jawab
-        </button>
+        <a class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+          Teamwork
+        </a>
       </h5>
     </div>
     <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
-      <div class="card-body">
-        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+      <div class="card-body ml-3">
+        <div class="form-check">
+         <input class="form-check-input" type="checkbox" value="10" name="aktif" id="defaultCheck1">
+         <label class="form-check-label" for="defaultCheck1">
+           Memberi saran,  pendapat, dan menjalankan tugas masing-masing ketika bekerja dalam tim
+         </label>
+       </div>
       </div>
     </div>
   </div>
 </div>
-<?php echo form_close();?>
