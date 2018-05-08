5333fdabcdfed83c6c5309b700fd479e2728c243
faishalrachman <faishalr97@gmail.com>
2018-05-08 22:11:16 +0700

    update HR

--- application/controllers/HR.php
+++ application/controllers/HR.php
@@ -1,8 +1,10 @@
 <?php
 	class HR extends SI_Controller{
 		public function index(){
-            $this->laman_hr('hr/v_dash');
-			
+		    $query = $this->db->query("SELECT NIP,nama_pg, divisi, sum(jum_poin) as jumlah from detil_poin JOIN poin using(id_poin) JOIN tb_pegawai using (NIP) JOIN tb_divisi using (id_div) GROUP BY NIP
+ORDER BY jumlah DESC");
+		    $data['daftarPoin'] = $query->result_array();
+            $this->laman_hr('hr/v_dash',$data);
 		}
 	}
 ?>
